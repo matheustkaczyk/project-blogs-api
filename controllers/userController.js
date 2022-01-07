e9f4b101ef614a3552704822b6b5befa946fb8e6
matheustkaczyk <matheus111904@hotmail.com>
2022-01-07 10:22:09 -0300

    criando validaçao do login, e logica de login e token

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 const { User } = require('../models');
-const userSchema = require('../validations/userPostValidation');
+const userCreateSchema = require('../validations/userPostValidation');
+const userLoginSchema = require('../validations/userLoginValidation');
 
 const token = require('../validations/jwt');
 
 router.post('/', async (req, res) => {
   try {
     const { displayName, email, password, image } = req.body;
-    const validation = userSchema.validate({ displayName, email, password, image });
+    const validation = userCreateSchema.validate({ displayName, email, password, image });
 
     if (validation.error) res.status(400).json({ message: validation.error.details[0].message });
 
@@ -25,6 +26,28 @@ router.post('/', async (req, res) => {
     const tokenJwt = token(newUser);
 
     res.status(201).json({ token: tokenJwt });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).end();
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const validation = userLoginSchema.validate({ email, password });
+
+    if (validation.error) res.status(400).json({ message: validation.error.details[0].message });
+
+    const userExists = await User.findAll({ where: { email } });
+
+    if (userExists.length <= 0) return res.status(400).json({ message: 'User not found' });
+
+    const user = { ...userExists[0].dataValues };
+
+    const tokenJwt = token(user);
+
+    res.status(200).json({ token: tokenJwt });
   } catch (error) {
     console.error(error.message);
     return res.status(500).end();
